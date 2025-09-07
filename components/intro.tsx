cd6e1475c4696049a77a8e0cf4e287167a5a873e
Ian C. Anderson <ian@iancanderson.com>
2025-09-07 13:26:01 -0400

    style: reduce top margin on Intro to tighten page top spacing

--- components/intro.tsx
+++ components/intro.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const Intro = () => {
   return (
-    <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
+    <section className="flex-col md:flex-row flex items-center md:justify-between mt-8 mb-12 md:mb-10">
       <h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
         <Link href="/">
           <a className="hover:underline">iancanderson</a>
