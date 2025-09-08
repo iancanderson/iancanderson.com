9f3f13906e37daaed6533beb287c7ee0c1ffe0a4
iancanderson <iancanderson@users.noreply.github.com>
2025-09-08 13:06:30 +0000

    deploy: 9fa2f1d78181873b5a873b81deb606c109432df3

---
+++ _next/static/SKKl35o41_KHXMgxPWELw/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002Fday\u002F[date]","\u002Fposts\u002F[slug]","\u002Ftags\u002F[tag]","\u002Fyear\u002F[year]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
