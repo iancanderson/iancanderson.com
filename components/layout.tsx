e99c41f2135164463f908f2d856d83db755ccb7c
Ian C. Anderson <ian@iancanderson.com>
2025-09-11 21:41:18 -0400

    Widen layout on xl+ to fit timeline (max-w-7xl)

--- components/layout.tsx
+++ components/layout.tsx
@@ -13,7 +13,7 @@ const Layout = ({ preview, children }: Props) => {
       <Meta />
       <div className="min-h-screen">
         {preview && <Alert />}
-        <main className="px-4 md:px-8 lg:px-12 max-w-5xl mx-auto">{children}</main>
+        <main className="px-4 md:px-8 lg:px-12 max-w-5xl xl:max-w-7xl mx-auto">{children}</main>
       </div>
       <Footer />
     </>
